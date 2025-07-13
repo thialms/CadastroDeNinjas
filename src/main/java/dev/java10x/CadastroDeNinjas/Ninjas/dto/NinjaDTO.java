336d8af9fccc1ff63c3749e2df5a445ea6c4b87f
thialms <thiiago.allmeida44@gmail.com>
2025-07-13 01:44:27 -0300

    chore: Include the DTO an Mapper in Ninja Entity

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/dto/NinjaDTO.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/dto/NinjaDTO.java
@@ -1,4 +1,24 @@
 package dev.java10x.CadastroDeNinjas.Ninjas.dto;
 
+import dev.java10x.CadastroDeNinjas.Missoes.model.MissoesModel;
+import dev.java10x.CadastroDeNinjas.Ninjas.model.enums.Clans;
+import dev.java10x.CadastroDeNinjas.Ninjas.model.enums.NinjaClass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaDTO {
+
+    private long id;
+    private String name;
+    private String email;
+    private  int age;
+    private Clans cla;
+    private NinjaClass ninjaClass;
+    private double jinchuuriki;
+    private MissoesModel missions;
+
 }
